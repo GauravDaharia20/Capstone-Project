e3986b763330789e7dd398577c48bbb608a859f6
GauravDaharia20 <gauravdaharia80@gmail.com>
2022-05-23 12:06:13 +0530

    new files

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
  import { Component, OnInit } from '@angular/core';
+import { Ingredients } from './../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,7 +8,10 @@
 })
 export class ShoppingListComponent implements OnInit {
 
-  ingredients =[];
+  ingredients:Ingredients[] =[
+    new Ingredients('Apples',5),
+    new Ingredients('Tomatoes',5)
+  ];
 
   constructor() { }
 
